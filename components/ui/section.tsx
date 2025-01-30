e5c9ca58a852a1676522db273330f3e14849b3fc
enoabasi <enoabasidream@gmail.com>
2025-01-30 17:31:24 +0100

    feat: add shadcn/ui components

---
+++ components/ui/section.tsx
@@ -0,0 +1,29 @@
+import React, { ComponentPropsWithoutRef } from "react";
+
+import { cn } from "@/lib/cn";
+
+interface Props extends ComponentPropsWithoutRef<"section"> {
+  bleed?: boolean;
+}
+
+export const Section: React.FC<Props> = ({
+  children,
+  className,
+  bleed = false,
+  ...props
+}) => {
+  return (
+    <section
+      className={cn(
+        "max-w-6xl mx-auto px-4 md:px-0 py-12 grid",
+        {
+          "max-w-full py-4": bleed,
+        },
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </section>
+  );
+};
